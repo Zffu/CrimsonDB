1cb1602398b99c067c9731feba364c180a5671c5
Zffu <radi0o0@outlook.com>
2024-04-09 19:53:08 +0200

    Initial commit

---
+++ src/main/java/net/zffu/crimson/CrimsonDatabase.java
@@ -0,0 +1,16 @@
+package net.zffu.crimson;
+
+import java.io.File;
+
+/**
+ * A Database that is powered by CrimsonDB.
+ * @author Zffu
+ */
+public class CrimsonDatabase {
+
+    /**
+     * The core folder of the database.
+     */
+    private File databaseFolder;
+
+}
